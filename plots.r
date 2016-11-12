ceba9965c372c0ba4566774ec47ec880a611ec20
Konstantina <Kspiliopoulou@hotmail.com>
2016-11-12 13:12:06 +0100

    sg

--- plots.r
+++ plots.r
@@ -8,6 +8,11 @@ library(plyr)
 library(Hmisc)
 library(base)
 library(ggplot2)
+library(RColorBrewer)
+library(reshape2)
+library(gridExtra)
+
+
 
 
 setwd("C:/Users/Konstantina/Desktop/Distance/Comparison/merged_tables")
@@ -20,104 +25,162 @@ plot(CSnE$Dist_CS, CSnE$Dist_E)
 
 
 #Merge with species info
-#Uber<- read.table("Uber_54Species_list.txt", h=T)
-#Compl<- merge(CSnE, Uber, by.x= "id_no", by.y= "num")
-#write.table(Compl, "CSnE_info.txt")
+	#Uber<- read.table("Uber_54Species_list.txt", h=T)
+	#Compl<- merge(CSnE, Uber, by.x= "id_no", by.y= "num")
+	#write.table(Compl, "CSnE_info.txt")
 
-Compl<- read.table("CSnE_info.txt", h=T)
+Compl<- read.table("CSnE_info2.txt", h=T)
+Compl$id_no<- as.factor(Compl$id_no)
 
 
 
+
+
+	
+	
 #IUCNhab
+out_path= "C:/Users/Konstantina/Desktop/Distance/Comparison/plots/IUCNhab"
 
-par(mfrow=c(3,2))
 for (i in unique(Compl$IUCNhab)){
- 
-	#a<- subset(Compl, IUCNhab==i)
-	print(ggplot(subset(Compl, IUCNhab==i),
-       aes(x=Dist_CS,
-           y=Dist_E,
-           colour=factor(subset(id_no, IUCNhab == i))))+
-  geom_point(aes(color= id_no)))
+
+	a<- subset(Compl, IUCNhab==i)
+	
+	cols = colorRampPalette(brewer.pal(12, "Paired"))
+	mypal = cols(length(unique(a$id_no)))
+	
+	b<- print(ggplot(a, aes(x=Dist_CS, y=Dist_E), color=id_no)+
+	geom_point(aes(color=id_no), size=1.2)+
+	scale_color_manual(values=mypal)+
+	labs(title = i, x = "Circuitscape", y = "Euclidean",colour="Species ID")+
+	coord_cartesian(xlim = c(0, 5),ylim=c(0,210000 ))+
+	theme(plot.title=element_text(face = "bold"), legend.title=element_text(face="italic"),
+	legend.key = element_rect(fill = "white"))+
+	guides(colour = guide_legend(override.aes = list(size=4))))
+	
+	
+	file_name = paste0(out_path,"/", i, ".tiff", sep="")
+	print(b,tiff(file_name, width= 3000, height=2000, units="px", res=300))
+	dev.off()
 	}
-  
-ggplot(subset(Compl, IUCNhab %in% c("Generalist")),aes(x=Dist_CS,y=Dist_E, color=unique(id_no)))+
-  geom_point()+
-  scale_fill_brewer(palette="Dark2")
-  #scale_fill_manual(aes(breaks = as.factor(unique(id_no))))
-  #geom_line(stat = "smooth", method = "loess")+
-  #scale_color_continuous(aes(name="",
-                        # breaks = unique(id_no)))
-                         #labels = c("22709196", "22710791", "22713365", "22715090", "22716487", "22718783", "22727786"),low = "green", high = "red")
-  
-  
- for (e in unique(a$id_no)){ 
- 
-	b<- subset(a,id_no==e)
-	print(i)
-	print(e)
-  
-  plot(a$Dist_CS, a$Dist_E, main=i, xlim=c(0,5), ylim=c(0,210000), xlab="Circuitscape", ylab="Euclidean", lines(lowess(a$Dist_CS, a$Dist_E),col="red"))
-	#pairs(b,panel=panel.smooth, col.line= "red")
-}
+
+
+
 
 	
 #GLCcodes
+out_path= "C:/Users/Konstantina/Desktop/Distance/Comparison/plots/GLCcodes"
 
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
 for (i in unique(Compl$GLCcodes)){
-  
-  a<- subset(Compl, GLCcodes==i)
-  print(i)
-  plot(a$Dist_CS, a$Dist_E, main=i, xlim=c(0,5), ylim=c(0,210000), xlab="Circuitscape", ylab="Euclidean")
-}
+
+	a<- subset(Compl, GLCcodes==i)
+	
+	cols = colorRampPalette(brewer.pal(12, "Paired"))
+	mypal = cols(length(unique(a$id_no)))
+	
+	b<- print(ggplot(a, aes(x=Dist_CS, y=Dist_E), color=id_no)+
+	geom_point(aes(color=id_no), size=1.2)+
+	scale_color_manual(values=mypal)+
+	labs(title = i, x = "Circuitscape", y = "Euclidean",colour="Species ID")+
+	coord_cartesian(xlim = c(0, 5),ylim=c(0,210000 ))+
+	theme(plot.title=element_text(face = "bold"), legend.title=element_text(face="italic"),
+	legend.key = element_rect(fill = "white"))+
+	guides(colour = guide_legend(override.aes = list(size=4))))
 	
 	
-#Uber
+	file_name = paste0(out_path,"/", i, ".tiff", sep="")
+	print(b,tiff(file_name, width= 3000, height=2000, units="px", res=300))
+	dev.off()
+	}
+	
+	
+	
+	
+#uber
+	
+out_path= "C:/Users/Konstantina/Desktop/Distance/Comparison/plots/Uber"
+
+for (i in unique(Compl$uber)){
+
+	a<- subset(Compl, uber==i)
+	
+	cols = colorRampPalette(brewer.pal(12, "Paired"))
+	mypal = cols(length(unique(a$id_no)))
+	
+	b<- print(ggplot(a, aes(x=Dist_CS, y=Dist_E), color=id_no)+
+	geom_point(aes(color=id_no), size=1.2)+
+	scale_color_manual(values=mypal)+
+	labs(title = i, x = "Circuitscape", y = "Euclidean",colour="Species ID")+
+	coord_cartesian(xlim = c(0, 5),ylim=c(0,210000 ))+
+	theme(plot.title=element_text(face = "bold"), legend.title=element_text(face="italic"),
+	legend.key = element_rect(fill = "white"))+
+	guides(colour = guide_legend(override.aes = list(size=4))))
+	
+	
+	file_name = paste0(out_path,"/", i, ".tiff", sep="")
+	print(b,tiff(file_name, width= 3000, height=2000, units="px", res=300))
+	dev.off()
+	}
+
 	
 
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-for (i in unique(Compl$Uber)){
-  
-  a<- subset(Compl, Uber==i)
-  print(i)
-  plot(a$Dist_CS, a$Dist_E, main=i, xlim=c(0,5), ylim=c(0,210000), xlab="Circuitscape", ylab="Euclidean")
-}
 	
-
-	
-
 #Order
 
+out_path= "C:/Users/Konstantina/Desktop/Distance/Comparison/plots/Order"
 
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
 for (i in unique(Compl$Order)){
-  
-  a<- subset(Compl, Order==i)
-  print(i)
-  plot(a$Dist_CS, a$Dist_E, main=i, xlim=c(0,5), ylim=c(0,210000), xlab="Circuitscape", ylab="Euclidean")
-}
-		
+
+	a<- subset(Compl, Order==i)
+	
+	cols = colorRampPalette(brewer.pal(12, "Paired"))
+	mypal = cols(length(unique(a$id_no)))
+	
+	b<- print(ggplot(a, aes(x=Dist_CS, y=Dist_E), color=id_no)+
+	geom_point(aes(color=id_no), size=1.2)+
+	scale_color_manual(values=mypal)+
+	labs(title = i, x = "Circuitscape", y = "Euclidean",colour="Species ID")+
+	coord_cartesian(xlim = c(0, 5),ylim=c(0,210000 ))+
+	theme(plot.title=element_text(face = "bold"), legend.title=element_text(face="italic"),
+	legend.key = element_rect(fill = "white"))+
+	guides(colour = guide_legend(override.aes = list(size=4))))
 	
 	
-#Dist
+	file_name = paste0(out_path,"/", i, ".tiff", sep="")
+	print(b,tiff(file_name, width= 3000, height=2000, units="px", res=300))
+	dev.off()
+	}
+
+	
+	
+#DistC
+
+out_path= "C:/Users/Konstantina/Desktop/Distance/Comparison/plots/DistC"
+
+for (i in unique(Compl$DistC)){
+
+	a<- subset(Compl, DistC==i)
+	
+	cols = colorRampPalette(brewer.pal(12, "Paired"))
+	mypal = cols(length(unique(a$id_no)))
+	
+	b<- print(ggplot(a, aes(x=Dist_CS, y=Dist_E), color=id_no)+
+	geom_point(aes(color=id_no), size=1.2)+
+	scale_color_manual(values=mypal)+
+	labs(title = i, x = "Circuitscape", y = "Euclidean",colour="Species ID")+
+	coord_cartesian(xlim = c(0, 5),ylim=c(0,210000 ))+
+	theme(plot.title=element_text(face = "bold"), legend.title=element_text(face="italic"),
+	legend.key = element_rect(fill = "white"))+
+	guides(colour = guide_legend(override.aes = list(size=4))))
+	
+	
+	file_name = paste0(out_path,"/", i, ".tiff", sep="")
+	print(b,tiff(file_name, width= 3000, height=2000, units="px", res=300))
+	dev.off()
+	}
 
 
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-par(mfrow=c(3,2))
-for (i in unique(Compl$Dist)){
-  
-  a<- subset(Compl, Dist==i)
-  print(i)
-  plot(a$Dist_CS, a$Dist_E, main=i, xlim=c(0,5), ylim=c(0,210000), xlab="Circuitscape", ylab="Euclidean")
-}
-		
-	
+
+
+
+
 	
